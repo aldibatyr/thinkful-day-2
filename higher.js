74026bab150e944dc6f33669172b071da6fa37fb
iAmDusan <dusaniwskyj@gmail.com>
2019-04-30 15:02:36 -0400

    added higher.js with higher-order functions

---
+++ higher.js
@@ -0,0 +1,19 @@
+/* eslint-disable indent */
+/* eslint-disable strict */
+
+
+function repeat(fn, n) {
+    for (let i = 0; i < fn.length; i++){
+        n(fn[i], i);
+    }
+
+function hello(){ 
+    console.log('Hello world'); 
+    }
+ 
+function goodgye(){
+     console.log('Goodbye world'); 
+    }
+
+console.log(repeat(hello, 5));
+}
