531b6ce2a05e26c93529647dfe3632ff8000d0ab
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-04-30 20:24:45 -0400

    fixed one missing semi colon

--- reduce.js
+++ reduce.js
@@ -3,12 +3,13 @@
 
 
 
-let string = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest'
+let string = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
 let array = string.split(' ').reduce((accum, word) => {
- if (word.length === 3) {
-   return accum + ' ';
- }
- return accum + word[word.length - 1].toUpperCase();
+  if (word.length === 3) {
+    return accum + ' ';
+  }
+  return accum + word[word.length - 1].toUpperCase();
 }, '');
 
+// eslint-disable-next-line no-console
 console.log(array);
